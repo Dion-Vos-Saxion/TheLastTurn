4fe577d2d233010c40bd75b53510d3717c1f0c45
Dion Vos <570548@student.saxion.nl>
2024-11-19 17:47:45 +0100

    Added base functionalities

--- project/BasicGame/src/BasicGame.java
+++ project/BasicGame/src/BasicGame.java
@@ -3,6 +3,8 @@ import nl.saxion.app.SaxionApp;
 import nl.saxion.app.interaction.GameLoop;
 import nl.saxion.app.interaction.KeyboardEvent;
 import nl.saxion.app.interaction.MouseEvent;
+import utils.MouseHandler;
+import utils.SceneManager;
 
 public class BasicGame implements GameLoop {
 
@@ -10,9 +12,13 @@ public class BasicGame implements GameLoop {
         SaxionApp.startGameLoop(new BasicGame(), 1000, 1000, 40);
     }
 
+    public MouseHandler mouseHandler;
+    public SceneManager sceneManager;
+
     @Override
     public void init() {
-
+        sceneManager = new SceneManager();
+        mouseHandler = new MouseHandler();
     }
 
     @Override
@@ -27,7 +33,7 @@ public class BasicGame implements GameLoop {
 
     @Override
     public void mouseEvent(MouseEvent mouseEvent) {
-
+        mouseHandler.update(mouseEvent);
     }
 }
 
---
+++ project/BasicGame/src/utils/MouseHandler.java
@@ -0,0 +1,10 @@
+package utils;
+
+import nl.saxion.app.interaction.MouseEvent;
+
+public class MouseHandler {
+
+    public void update(MouseEvent mouseEvent) {
+
+    }
+}
