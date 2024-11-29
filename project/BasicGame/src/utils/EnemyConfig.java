5dfd2487c88cefabb7f4b857124a4ba0e7ef55da
sysreqctrl <569517@student.saxion.nl>
2024-11-29 11:28:32 +0100

    Extra changes to enemyconfig for when player is in game.
    while loop and update loop changes

--- project/BasicGame/src/utils/EnemyConfig.java
+++ project/BasicGame/src/utils/EnemyConfig.java
@@ -11,11 +11,17 @@ public class EnemyConfig extends BaseEnemy {
 
     @Override
     public void loop() {
-
+        // while player is attacking and blocking capability is false
+        // isDamaged = true
+        // damageAmount = player.attackStat ( ? )
+        // if blocking capability = true
+        // print blocked
     }
 
     @Override
     public void update() {
-
+        // while enemy has been attacked
+        // update enemy hp
+        // update enemy
     }
 }
