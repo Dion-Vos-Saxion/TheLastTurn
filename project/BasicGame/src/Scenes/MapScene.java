f5ef8ad361cad29a9d0e5388428314b105e9e0f2
sysreqctrl <569517@student.saxion.nl>
2024-12-04 13:51:15 +0100

    Update MapScene.java

--- project/BasicGame/src/Scenes/MapScene.java
+++ project/BasicGame/src/Scenes/MapScene.java
@@ -7,7 +7,6 @@ public class MapScene extends Scene{
     public MapScene() {
         gameObjects = new GameObject[12];
         gameObjects[0] = new GameImage(512, 512, 1024,1024, "");
-
     }
 
 
