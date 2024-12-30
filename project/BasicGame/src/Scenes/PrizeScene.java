5f650e34873497b26b819540bbe2882fd9d26d93
Dion Vos <570548@student.saxion.nl>
2024-12-30 18:15:13 +0100

    added more UI elements

---
+++ project/BasicGame/src/Scenes/PrizeScene.java
@@ -0,0 +1,18 @@
+package Scenes;
+
+import utils.Scene;
+
+public class PrizeScene extends Scene {
+
+    public void init() {
+
+    }
+
+    public void loop() {
+
+    }
+
+    public void unInit() {
+
+    }
+}
